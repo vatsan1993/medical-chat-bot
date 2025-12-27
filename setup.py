730bdba610e7d21c29431b3c607c7bc74e9964a7
Sozhavaram Srivatasan <sozhavaramsrivatsan@hotmail.com>
2025-12-27 19:20:40 +0530

    added folder structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup (
+    name = "Medical Chatbot Project",
+    version = "0.0.1",
+    author = "Srivatsan",
+    author_email = "sozhavaramsrivatsan@hotmail.com",
+    packages = find_packages(),
+    install_requires = [
+
+    ]
+)
